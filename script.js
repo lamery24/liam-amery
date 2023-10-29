9f8712e8e1bf8a3e5c703ef8aed1c296b9444522
lamery24 <93548465+lamery24@users.noreply.github.com>
2023-10-29 14:32:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+// Get the sidebar element
+const sidebar = document.querySelector('.sidebar');
+
+// Get the initial offset position of the sidebar
+const sidebarOffset = sidebar.offsetTop;
+
+// Function to handle scrolling and fix/unfix the sidebar
+function fixSidebar() {
+    if (window.pageYOffset >= sidebarOffset) {
+        sidebar.style.position = 'fixed';
+    } else {
+        sidebar.style.position = 'absolute';
+    }
+}
+
+// Add event listener for scroll event
+window.addEventListener('scroll', fixSidebar);
+In this code:
+
+The margin-left property in the CSS sets the initial margin, creating space for the sidebar.
+The JavaScript code uses the scroll event to detect scrolling. When the user scrolls, the fixSidebar function checks if the window's scroll position is greater than or equal to the initial offset position of the sidebar. If it is, the sidebar's position is set to fixed, making it fixed while scrolling. If the scroll position is less than the initial offset, the sidebar's position is set to absolute, allowing it to scroll with the content.
+The transition property in the CSS adds a smooth transition effect to the margin-left property, creating a smooth movement when the sidebar changes its position.
+Adjust the values in the CSS and JavaScript according to your layout and design preferences.
+
+
+
+
+
 // You can add JavaScript code for interactive elements here
