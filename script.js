0bef0759d4419c6d0b03622bec3aef739be1762c
lamery24 <93548465+lamery24@users.noreply.github.com>
2023-10-29 15:25:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-let close_btn = document.querySelector('#close')
-        let ham = document.querySelector('#ham')
 
-        let nav = document.querySelector('.side-nav')
-
-        close_btn.addEventListener('click',()=>{
-            nav.style.display = "none"
-            ham.style.display = "block"
-
-        })
-        ham.addEventListener('click',()=>{
-            nav.style.display = "block"
-            ham.style.display = "none"
-
-        })
